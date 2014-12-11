93f2efea43eea0ba51ecd634f6592f0991e8f7ab
cloughit <cloughit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-11 00:52:09 +0000

    * Updated minor revision issue
    git-svn-id: https://plugins.svn.wordpress.org/async-javascript/trunk@1042348 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,23 @@
+/*
+Plugin Name: Async Javascript
+Plugin URI: http://www.cloughit.com.au/wordpress/plugins/async-javascript-wordpress-plugin/
+Description: Async Javascript adds a 'async' or 'defer' attribute to scripts loaded via wp_enqueue_script
+Version: 1.14.12.11
+Author: David Clough (cloughit)
+Author URI: http://www.cloughit.com.au/
+Text Domain: async-javascript
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+/**
+ *	isChecked()
+ *
+ *	checks to see if the 'aj_enabled' checkbox is checked.  If yes, show extended options, if no, hide extended options
+ *
+ *  @param		n/a
+ *	@return		n/a
+ */
 function isChecked() {
 	if (jQuery('#aj_enabled').prop('checked')) {
 		jQuery('.aj_method').show();
@@ -6,6 +26,9 @@ function isChecked() {
 	}
 }
 
+/**
+ *	functions and actions to load after document ready
+ */
 jQuery(document).ready(function() {
 	isChecked();
 
